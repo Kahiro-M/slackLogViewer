684ac08fbd57f2aa92271443f2bd4f57b4c138b7
Kahiro-M <0204.kahiro@gmail.com>
2024-04-16 08:16:56 +0000

    検索結果から該当の会話へのリンクを追加

--- viewer_channel.php
+++ viewer_channel.php
@@ -27,34 +27,36 @@ $channelName = get_channel_name($db, $channelCode);
 
 <?php include('./template/sidebar.php'); ?>
 
-<div class="msg_body">
+<div class="msg_view">
     <div class="ch_title">
         <?php print($channelName); ?><span class="channel_code"> (<?php print($channelCode); ?>)</span>
-<?php if(empty($channelCode)){ ?>
+    <?php if(empty($channelCode)){ ?>
     ←からチャンネルを選択してください。
-<?php } ?>
+    <?php } ?>
     </div>
+    <div class="msg_body">
 <?php 
 $results = get_channel_msgs($db,$channelName,$orderby);
 while ($row = $results->fetchArray()) {
 ?>
-    <div class="msg">
-        <img src="<?php if(!empty(get_user_id($db,$row['name']))){print('./img/user_icon/'.get_user_id($db,$row['name']).'.jpg');}else{print('/img/default_icon.png');} ?>" class="user_icon">
-        <div class="msg_info">
-            <div class="msg_name"><?php print($row['name']);?></div>
-            <div class="msg_timestamp"><?php print($row['timestamp']);?></div>
-            <div class="msg_channel"><?php print($row['channel']);?></div>
-        </div>
-        <div class="msg_content">
+        <div class="msg" id="<?php print($row['msgid']);?>">
+            <img src="<?php if(!empty(get_user_id($db,$row['name']))){print('./img/user_icon/'.get_user_id($db,$row['name']).'.jpg');}else{print('/img/default_icon.png');} ?>" class="user_icon">
+            <div class="msg_info">
+                <div class="msg_name"><?php print($row['name']);?></div>
+                <div class="msg_timestamp"><?php print($row['timestamp']);?></div>
+                <div class="msg_channel"><?php print($row['channel']);?></div>
+            </div>
+            <div class="msg_content">
 <?php   if(!empty($row['files'])){ ?>
-            <a href="<?php print($row['files']);?>">添付ファイル</a><br>
+                <a target="_blank" href="<?php print($row['files']);?>">添付ファイル</a><br>
 <?php   }?>
-        <?php print(nl2br(replace_http_to_link($row['text'])));?>
-    </div>
-    </div>
+                <?php print(nl2br(replace_http_to_link($row['text'])));?>
+            </div>
+        </div>
 <?php }?>
 <div id="eom" class="msg"></div>
-</div> <!-- class="msg_body"> -->
+    </div> <!-- class="msg_body"> -->
+</div> <!-- class="msg_view"> -->
 
 <?php 
 include('./template/footer.php');
--- viewer_search.php
+++ viewer_search.php
@@ -23,35 +23,46 @@ include('./common/db.php');
 include('./template/header.php');
 
 $channelName = '検索結果';
+$results = search_channel_msgs($db,$searchWord,$orderby);
+$count = 0;
+while ($row = $results->fetchArray()) {
+    $count++;
+}
 ?>
 
 <?php include('./template/sidebar.php'); ?>
 
-<div class="msg_body">
+<div class="msg_view">
     <div class="ch_title">
-        <?php print($channelName); ?><span class="channel_code"> ()</span>
+        <?php print($channelName); ?> : <?php print($count); ?><span class="channel_code"> ()</span>
     </div>
+
+    <div class="msg_body">
 <?php 
-$results = search_channel_msgs($db,$searchWord,$orderby);
+$results->reset();
 while ($row = $results->fetchArray()) {
 ?>
-    <div class="msg">
-        <img src="<?php if(!empty(get_user_id($db,$row['name']))){print('./img/user_icon/'.get_user_id($db,$row['name']).'.jpg');}else{print('/img/default_icon.png');} ?>" class="user_icon">
-        <div class="msg_info">
-            <div class="msg_name"><?php print($row['name']);?></div>
-            <div class="msg_timestamp"><?php print($row['timestamp']);?></div>
-            <div class="msg_channel"><?php print($row['channel']);?></div>
-        </div>
-        <div class="msg_content">
+        <a class="serach_link" href="viewer_channel.php?ch=<?php print(get_channel_code($db, $row['channel']));?>#<?php print($row['msgid']);?>">
+
+            <div class="msg">
+                <img src="<?php if(!empty(get_user_id($db,$row['name']))){print('./img/user_icon/'.get_user_id($db,$row['name']).'.jpg');}else{print('/img/default_icon.png');} ?>" class="user_icon">
+                <div class="msg_info">
+                    <div class="msg_name"><?php print($row['name']);?></div>
+                    <div class="msg_timestamp"><?php print($row['timestamp']);?></div>
+                    <div class="msg_channel"><?php print($row['channel']);?></div>
+                </div>
+                <div class="msg_content">
 <?php   if(!empty($row['files'])){ ?>
-            <a href="<?php print($row['files']);?>">添付ファイル</a><br>
+                    <a target="_blank" href="<?php print($row['files']);?>">添付ファイル</a><br>
 <?php   }?>
-        <?php print(nl2br(replace_http_to_link($row['text'])));?>
-    </div>
-    </div>
+                    <?php print(nl2br(replace_http_to_link($row['text'])));?>
+                </div>
+            </div>
+        </a>
 <?php }?>
 <div id="eom" class="msg"></div>
-</div> <!-- class="msg_body"> -->
+    </div> <!-- class="msg_body"> -->
+</div> <!-- class="msg_view"> -->
 
 <?php 
 include('./template/footer.php');
